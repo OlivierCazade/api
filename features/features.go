5ef09cc142fb0ec77e16c69769a30dd1f1a6b4a4
Olivier Cazade <ocazade@redhat.com>
2026-03-15 17:23:07 +0100

    config/v1: add networkObservability field for day-0 installation
    Add networkObservability nested structure to configure network observability
    installation during cluster deployment (day-0).
    
    - Add NetworkObservability field to NetworkSpec with NetworkObservabilitySpec type
    - Add NetworkObservabilityInstallationPolicy enum ("", "InstallAndEnable", "DoNotInstall")
    - Add NetworkObservabilityInstall feature gate enabled in DevPreview and TechPreview
    - Add integration test suite for the new field
    
    When installationPolicy is set to "" (empty string) or "InstallAndEnable",
    network observability will be installed and enabled. When set to "DoNotInstall",
    it will not be installed.
    
    Related: https://github.com/openshift/enhancements/pull/1908

--- features/features.go
+++ features/features.go
@@ -1033,4 +1033,20 @@ var (
 					enhancementPR("https://github.com/openshift/enhancements/pull/1933").
 					enable(inDevPreviewNoUpgrade(), inTechPreviewNoUpgrade()).
 					mustRegister()
+
+	FeatureGateTLSAdherence = newFeatureGate("TLSAdherence").
+				reportProblemsToJiraComponent("HPCASE / TLS Adherence").
+				contactPerson("joelanford").
+				productScope(ocpSpecific).
+				enhancementPR("https://github.com/openshift/enhancements/pull/1910").
+				enable(inDevPreviewNoUpgrade(), inTechPreviewNoUpgrade()).
+				mustRegister()
+
+	FeatureGateNetworkObservabilityInstall = newFeatureGate("NetworkObservabilityInstall").
+						reportProblemsToJiraComponent("netobserv").
+						contactPerson("jtakvori").
+						productScope(ocpSpecific).
+						enhancementPR("https://github.com/openshift/enhancements/pull/1908").
+						enable(inDevPreviewNoUpgrade(), inTechPreviewNoUpgrade()).
+						mustRegister()
 )
